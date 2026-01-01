0d56976d28bb611c0b85a10f668cb8065bd0fc20
emmanuelafadzegit <emmanuelafadze@yahoo.com>
2026-01-01 09:10:26 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,6 @@
 console.log("auth.js loaded");
+
+/* LOGIN */
 const loginForm = document.getElementById("loginForm");
 
 if (loginForm) {
@@ -22,3 +24,33 @@ if (loginForm) {
   });
 }
 
+/* SIGN UP */
+const signupForm = document.getElementById("signupForm");
+
+if (signupForm) {
+  signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const password = document.getElementById("signupPassword").value;
+    const confirm = document.getElementById("confirmPassword").value;
+
+    if (password !== confirm) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    const email = document.getElementById("signupEmail").value;
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    alert("Check your email to confirm your account.");
+  });
+}
