0642be3555bfbcce2bb6426cea1c854ea7813ccf
emmanuelafadzegit <emmanuelafadze@yahoo.com>
2026-01-01 10:19:22 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-const SUPABASE_URL = "https://zuotycdpvlcueygrvtix.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1b3R5Y2RwdmxjdWV5Z3J2dGl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyMzQwMjMsImV4cCI6MjA4MjgxMDAyM30.nF0BVNCESM7eTKnaILYfXqdlSA0TT_ZD4OSCNzOlMNI";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+// supabase.js
+// Only declare supabase once globally
+window.supabase = window.supabase || supabase.createClient(
+  "https://zuotycdpvlcueygrvtix.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1b3R5Y2RwdmxjdWV5Z3J2dGl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyMzQwMjMsImV4cCI6MjA4MjgxMDAyM30.nF0BVNCESM7eTKnaILYfXqdlSA0TT_ZD4OSCNzOlMNI"
 );
